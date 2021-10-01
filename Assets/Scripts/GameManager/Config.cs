d12dbf4c67ce9cf956dd73ae18d24b98bf82e735
SebastianZambranoR <sebastiane11qglk2@gmail.com>
2021-10-01 00:13:24 -0500

    Se creo el GameManager y se establecio el ratio de frames

---
+++ Assets/Scripts/GameManager/Config.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Config : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.targetFrameRate = 80;//limita el framerate a 80 fps
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
